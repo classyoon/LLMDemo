d50a5111abaf66d3da0125053d69fc6cc924b2b9
classyoon <96508736+classyoon@users.noreply.github.com>
2026-01-17 21:11:16 -0500

    Initial Commit

---
+++ LLMDemo/LLMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMDemoApp.swift
+//  LLMDemo
+//
+//  Created by Conner Yoon on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct LLMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LLMDemoTests/LLMDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LLMDemoTests.swift
+//  LLMDemoTests
+//
+//  Created by Conner Yoon on 1/17/26.
+//
+
+import Testing
+@testable import LLMDemo
+
+struct LLMDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
